23888354b98f2c331638c903eff0349b0eba1da7
Francesco Fiori <francesco.fiori@gmail.com>
2017-12-12 00:06:49 +0000

    added browserrouter basename

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router} from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+import {BrowserRouter} from 'react-router-dom';
 
 import 'normalize.css';
 import 'font-awesome/css/font-awesome.css';
@@ -10,16 +9,14 @@ import App from './App';
 import configureStore from './store/config.store';
 import DevTools from './components/DevTools/DevTools';
 
-const customHistory = createBrowserHistory();
-
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
     <div>
-      <Router history={customHistory}>
+      <BrowserRouter basename={process.env.NODE_ENV === 'development' ? '/' : '/trainline-client'}>
         <App />
-      </Router>
+      </BrowserRouter>
       <DevTools />
     </div>
   </Provider>,
